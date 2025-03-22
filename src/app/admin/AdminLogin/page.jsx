6a7189df2f2ba01f05f55edccf167bbff524b7e2
varsha016 <vharkal16@gmail.com>
2025-03-22 12:43:53 +0530

    update or condition

--- src/app/admin/AdminLogin/page.jsx
+++ src/app/admin/AdminLogin/page.jsx
@@ -76,7 +76,8 @@ const AdminLogin = () => {
     const [password, setPassword] = useState("123");
     const [error, setError] = useState("");
     const router = useRouter();
-    const baseURL = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:3000/api';
+    // const baseURL = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:3000/api';
+    const baseURL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
     console.log('Backend URL:', process.env.NEXT_PUBLIC_BACKEND_URL);
 
 
