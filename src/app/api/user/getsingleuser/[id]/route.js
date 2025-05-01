8a6874d118f84246681830dfb75a825376a4ef66
varsha016 <vharkal16@gmail.com>
2025-05-01 11:02:37 +0530

    update bcrypt password and other pages solve logs

--- src/app/api/user/getsingleuser/[id]/route.js
+++ src/app/api/user/getsingleuser/[id]/route.js
@@ -1,4 +1,4 @@
-import connectDB from "../../../lib/db";
+import connectDB from "../../../../lib/db";
 import User from "../../../models/user/UserModel";
 import { NextResponse } from "next/server";
 export async function GET(req, { params }) {
